e4cf6d0238317974b0d8db57f1750652e35f3de8
nicoletta777 <n.parras23@uniandes.edu.co>
2026-05-18 19:57:04 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
+// GLITCH RANDOM
+setInterval(() => {
+  document.body.style.filter = "hue-rotate(90deg)";
 
+  setTimeout(() => {
+    document.body.style.filter = "none";
+  }, 100);
+}, 4000);
+
+// EFECTO PARPADEO
+setInterval(() => {
+  document.body.style.opacity = "0.8";
+
+  setTimeout(() => {
+    document.body.style.opacity = "1";
+  }, 100);
+}, 5000);
